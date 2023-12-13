9fa1d3886a08ecc9d87da5c2921d3be63b1e1a63
qweku2000 <joeadofonyarko@gmail.com>
2023-12-13 06:14:28 -0800

    No mem leaks

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include "shell.h"
 
-int main() {
-    while (1) {
+int main()
+{
+    while (1)
+      {
         prompt_line();
-    }
+      }
     return 0;
 }
 
@@ -13,65 +15,74 @@ void prompt_line() {
     ssize_t getline_bytes;
     size_t n = 0;
     char **commands = NULL;
-    int i;
-
+    int i,j,num_commands;
+    char* command = NULL;
     /* Print prompt */
-    if (isatty(fileno(stdin))) {
+    if (isatty(fileno(stdin)))
+      {
         char buf[1064];
-        if (getcwd(buf, sizeof(buf)) != NULL) {
+        if (getcwd(buf, sizeof(buf)) != NULL)
+	  {
             write(STDOUT_FILENO, buf, strlen(buf));
             write(STDOUT_FILENO, "\n$ ", 3);
-        } else {
+	  }
+	else
+	  {
             perror("Error: cwd error");
-        }
-    }
+	  }
+      }
 
     /* Read input from user */
     getline_bytes = getline(&buffer_original, &n, stdin);
-    if (getline_bytes == -1) {
+    if (getline_bytes == -1)
+      {
         free(buffer_original);
         exit(0);
-    }
-    if (getline_bytes == 1 && buffer_original[0] == '\n') {
+      }
+    if (getline_bytes == 1 && buffer_original[0] == '\n')
+      {
         free(buffer_original);
         return;
-    }
+      }
 
     buffer_copy = strdup(buffer_original);
     hashes(buffer_copy);
-
+    
     /* Tokenize string based on ; */
-    char *command = strtok(buffer_original, ";\n");
-    int num_commands = 0;
-
-    while (command != NULL) {
+    command = strtok(buffer_original, ";\n");
+    num_commands = 0;
+    
+    while (command != NULL)
+      {
         commands = realloc(commands, (num_commands + 1) * sizeof(char *));
         commands[num_commands] = strdup(command);
         num_commands++;
         command = strtok(NULL, ";\n");
-    }
-
+      }
+    
     /* Execute each command */
-    for (i = 0; i < num_commands; i++) {
+    for (i = 0; i < num_commands; i++)
+      {
         char **string_arr = NULL;
         int no_tokens = 0;
-
+	
         /* Tokenize each command based on space */
         char *token = strtok(commands[i], " \n");
-        while (token != NULL) {
+        while (token != NULL)
+	  {
             string_arr = realloc(string_arr, (no_tokens + 1) * sizeof(char *));
             string_arr[no_tokens] = strdup(token);
             no_tokens++;
             token = strtok(NULL, " \n");
-        }
+	  }
 
         /* Execute the command */
         string_arr = realloc(string_arr, (no_tokens + 1) * sizeof(char *));
-        string_arr[no_tokens] = NULL;  // Null-terminate the array
+        string_arr[no_tokens] = NULL;  /*Null-terminate the array*/
         execute(string_arr);
 
         /* Free memory */
-        for (int j = 0; j <= no_tokens; j++)
+        for (j = 0; j <= no_tokens; j++)
 	  {
             free(string_arr[j]);
 	  }
